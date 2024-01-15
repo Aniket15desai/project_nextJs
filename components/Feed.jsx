d80e8eaecc791f73f0d1fc9cb903846e9dea8f09
aniket15desai <aniketsardesai44@gmail.com>
2024-01-16 01:51:38 +0530

    Changes

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,21 +1,35 @@
 "use client";
-import { useState, useEffect } from 'react'
-import PromptCard from './PromptCard'
+import { useState, useEffect } from 'react';
+import PromptCard from './PromptCard';
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className='mt-16 prompt_layout'>
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  );
+};
 
 const Feed = () => {
   const [searchText, setSearchText] = useState('');
-  const [posts, setPosts] = useState([])
+  const [allPosts, setAllPosts] = useState([])
 
   const handleSearchChange = (e) => {
 
   }
+  const fetchPost = async () => {
+    const response = await fetch(`/api/prompt`);
+    const data = await response.json();
+    setAllPosts(data);
+  }
 
   useEffect(() => {
-    const fetchPost = async () => {
-      const response = await fetch(`/api/prompt`);
-      const data = await response.json();
-      setPosts(data);
-    }
     fetchPost();
   }, [])
 
@@ -31,17 +45,10 @@ const Feed = () => {
           className='search_input peer'
         />
       </form>
-      <div className='mt-16 prompt_layout'>
-        {posts.map((post) => {
-          return (
-            <PromptCard
-              key={post._id}
-              post={post}
-              handleTagClick={() => { }}
-            />
-          )
-        })}
-      </div>
+      <PromptCardList
+        data={allPosts}
+        handleTagClick={() => { }}
+      />
     </section>
   )
 }
